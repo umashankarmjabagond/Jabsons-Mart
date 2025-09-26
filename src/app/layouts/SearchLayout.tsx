304b4409b13fad424a1e560c324ea897d635bece
umashankarmjabagond <umashankarjabagond@gmail.com>
2025-09-26 13:05:08 +0530

    fix : add missed code in home filter.

--- src/app/layouts/SearchLayout.tsx
+++ src/app/layouts/SearchLayout.tsx
@@ -4,6 +4,77 @@ import RequirementForm from "@/features/productList/RequirementForm";
 import Footer from "@/components/common/Footer";
 import FilterSlideBar from "@/features/productList/FilterSlideBar";
 import Navbar from "@/components/common/Navbar";
+import { SidebarSection } from "@/types/sidebarTypes";
+
+const sidebarData: SidebarSection[] = [
+  {
+    type: "text",
+    title: "Price",
+    options: [
+      "₹2,000 and Below",
+      "₹2,001-₹9,000",
+      "₹9,001-₹50,000",
+      "₹50,001 and Above",
+    ],
+    showRange: true,
+  },
+  {
+    type: "filter",
+    title: "Filters",
+    options: ["Bengaluru-based Suppliers"],
+  },
+  {
+    type: "text",
+    title: "Usage/Application",
+    options: ["Industrial", "Garage"],
+  },
+  {
+    type: "text",
+    title: "Business Type",
+    options: ["Manufacturer", "Exporter", "Wholesaler", "Retailer"],
+  },
+  {
+    type: "text",
+    title: "Related Category",
+    options: [
+      {
+        label: "Agriculture Production Services",
+        image:
+          "https://3.imimg.com/data3/FN/KW/GLADMIN-171253/agriculture-production-services-125x125.jpg",
+      },
+      {
+        label: "Vermicompost",
+        image:
+          " https://3.imimg.com/data3/CT/UA/GLADMIN-2728/vermicompost-125x125.jpg",
+      },
+      {
+        label: "Organic Fertilizers and Manure",
+        image:
+          "https://3.imimg.com/data3/CS/YS/GLADMIN-156355/vesicular-arbuscular-mycorrhiza-125x125.jpg"
+
+      },
+      {
+        label: "Agricultural Pesticides",
+        image:
+          "https://3.imimg.com/data3/FU/EW/GLADMIN-3179/agricultural-pesticides-125x125.jpg",
+      },
+      {
+        label: "Press Mud",
+        image:
+          "https://3.imimg.com/data3/YU/RG/GLADMIN-69082/press-mud-125x125.jpg",
+      },
+      {
+        label: "Green Manure",
+        image: "https://3.imimg.com/data3/SR/OL/MY-2/green-manure-125x125.jpg",
+      },
+    ],
+  },
+  {
+    type: "text",
+    title: "Recommended Searches",
+    options: ["agriculture", "agriculture seating"],
+  },
+];
 
 const SearchLayout: React.FC = () => {
   const [showForm, setShowForm] = useState(false);
@@ -40,16 +111,15 @@ const SearchLayout: React.FC = () => {
           📍 Location: Bangalore, India
         </nav>
 
-      <div className="flex-[8] flex w-full overflow-hidden">
-        <aside className="w-1/5 border-2 p-4">
-           <FilterSlideBar />
-        </aside>
-        <main
-          ref={mainRef}
-          className="w-4/5 border-2 h-full overflow-y-auto flex flex-col"
-        >
-          <ProductList />
-
+        <div className="flex-[8] flex w-full overflow-hidden">
+          <aside className="w-1/5 p-1">
+            <FilterSlideBar sidebarData={sidebarData} />
+          </aside>
+          <main
+            ref={mainRef}
+            className="w-4/5 border-2 h-full overflow-y-auto flex flex-col"
+          >
+            <ProductList />
 
             <div ref={productListEndRef} className="h-4" />
 
--- src/features/productlist/FilterSlideBar.tsx
+++ src/features/productlist/FilterSlideBar.tsx
@@ -1,85 +1,20 @@
 import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import { Menu, X } from "lucide-react"; 
+import { Menu, X } from "lucide-react";
 import FilterDropdown from "@/components/common/FilterSection";
 import TextDropdown from "@/components/common/TextDropdown";
 import { SidebarSection } from "@/types/sidebarTypes";
-const sidebarData: SidebarSection[] = [
-  {
-    type: "text",
-    title: "Price",
-    options: [
-      "₹2,000 and Below",
-      "₹2,001-₹9,000",
-      "₹9,001-₹50,000",
-      "₹50,001 and Above",
-    ],
-    showRange: true,
-  },
-  {
-    type: "filter",
-    title: "Filters",
-    options: ["Bengaluru-based Suppliers"],
-  },
-  {
-    type: "text",
-    title: "Usage/Application",
-    options: ["Industrial", "Garage"],
-  },
-  {
-    type: "text",
-    title: "Business Type",
-    options: ["Manufacturer", "Exporter", "Wholesaler", "Retailer"],
-  },
-  {
-    type: "text",
-    title: "Related Category",
-    options: [
-      {
-        label: "Agriculture Production Services",
-        image:
-          "https://3.imimg.com/data3/FN/KW/GLADMIN-171253/agriculture-production-services-125x125.jpg",
-      },
-      {
-        label: "Vermicompost",
-        image:
-          "	https://3.imimg.com/data3/CT/UA/GLADMIN-2728/vermicompost-125x125.jpg",
-      },
-      {
-        label: "Organic Fertilizers and Manure",
-        image:
-          "https://5.imimg.com/data5/GLADMIN/Default/2022/6/XM/YY/PN/92368/organic-fertilizers-and-manure-125x125.jpg"
-      },
-      {
-        label: "Agricultural Pesticides",
-        image:
-          "https://3.imimg.com/data3/FU/EW/GLADMIN-3179/agricultural-pesticides-125x125.jpg",
-      },
-      {
-        label: "Press Mud",
-        image:
-          "https://3.imimg.com/data3/YU/RG/GLADMIN-69082/press-mud-125x125.jpg",
-      },
-      {
-        label: "Green Manure",
-        image:"https://3.imimg.com/data3/SR/OL/MY-2/green-manure-125x125.jpg",
-      },
-    ],
-  },
-  {
-    type: "text",
-    title: "Recommended Searches",
-    options: ["agriculture", "agriculture seating"],
-  },
-];
 
+interface FilterSlideBarProps {
+  sidebarData: SidebarSection[];
+}
 
-const FilterSlideBar: React.FC = () => {
+const FilterSlideBar: React.FC<FilterSlideBarProps> = ({ sidebarData }) => {
   const [isOpen, setIsOpen] = useState(false);
-
+  console.log("sidebarData in filter page", sidebarData)
   return (
     <>
-      <div className="sm:hidden p-2">
+      <div className="lg:hidden p-2">
         <button
           onClick={() => setIsOpen(true)}
           className="p-2 border rounded-md bg-white shadow"
@@ -88,8 +23,8 @@ const FilterSlideBar: React.FC = () => {
         </button>
       </div>
 
-      <aside className="hidden sm:flex w-60 bg-white border rounded-md border-gray-200 flex-col h-screen  overflow-y-auto">
-        {sidebarData.map((section) =>
+      <div className="hidden lg:block w-60 bg-white border border-gray-200 rounded-md flex-col h-full max-h-full overflow-y-auto">
+        {sidebarData?.map((section) =>
           section.type === "text" ? (
             <TextDropdown
               key={section.title}
@@ -105,51 +40,42 @@ const FilterSlideBar: React.FC = () => {
             />
           )
         )}
-      </aside>
+      </div>
 
       <AnimatePresence>
         {isOpen && (
-          <>
-            <motion.div
-              className="fixed inset-0 bg-black bg-opacity-50 z-40"
+          <motion.div
+            initial={{ x: "-100%" }}
+            animate={{ x: 0 }}
+            exit={{ x: "-100%" }}
+            transition={{ duration: 0.3 }}
+            className="fixed inset-y-0 left-0 w-64 bg-white shadow-lg z-50 p-4 overflow-y-auto"
+          >
+            <button
               onClick={() => setIsOpen(false)}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-            />
-
-            <motion.aside
-              className="fixed top-0 left-0 w-full h-full bg-white border-r border-gray-200 z-50 flex flex-col p-4 overflow-y-auto"
-              initial={{ x: "-100%" }}
-              animate={{ x: 0 }}
-              exit={{ x: "-100%" }}
-              transition={{ type: "tween", duration: 0.3 }}
+              className="mb-4 flex items-center space-x-2"
             >
-              <button
-                onClick={() => setIsOpen(false)}
-                className="mb-4 self-end p-2 border rounded-md bg-gray-100"
-              >
-                <X size={20} />
-              </button>
+              <X className="w-6 h-6" />
+              <span>Close</span>
+            </button>
 
-              {sidebarData.map((section) =>
-                section.type === "text" ? (
-                  <TextDropdown
-                    key={section.title}
-                    title={section.title}
-                    options={section.options}
-                    showRange={section.showRange}
-                  />
-                ) : (
-                  <FilterDropdown
-                    key={section.title}
-                    title={section.title}
-                    options={section.options}
-                  />
-                )
-              )}
-            </motion.aside>
-          </>
+            {sidebarData.map((section) =>
+              section.type === "text" ? (
+                <TextDropdown
+                  key={section.title}
+                  title={section.title}
+                  options={section.options}
+                  showRange={section.showRange}
+                />
+              ) : (
+                <FilterDropdown
+                  key={section.title}
+                  title={section.title}
+                  options={section.options}
+                />
+              )
+            )}
+          </motion.div>
         )}
       </AnimatePresence>
     </>
