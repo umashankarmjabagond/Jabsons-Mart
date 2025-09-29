047c31692f804a2a64d4a5db663c61e10f8a3164
Kavya R <kavyaryadhav@gmail.com>
2025-09-29 16:02:18 +0530

    navbar implemented for dashboard

--- src/app/Dashboard.tsx
+++ src/app/Dashboard.tsx
@@ -1,13 +1,11 @@
-import CommonCarousel from "@/components/common/CommonCarousel";
-import { products } from "../utils/json_util";
-import { DASHBOARD_TEXT, HEADING, SERVICES } from "@constants/textConstants";
-import { SlideItem } from "@/types/carouselTypes";
+
 import Testimonials from "@/components/common/Testimonials";
 import UseCategory from "@/features/dashboard/productCategory/UseCategory";
 import Footer from "@/components/common/Footer";
 import MoreForYouCard from "@/components/common/MoreForYouCard";
 import DashboardCarousel from "@/components/DashboardCarousel";
 import DashboardNav from "@/components/DashboardNav";
+import { HEADING, SERVICES } from "@/constants/textConstants";
 function Dashboard() {
   return (
     <>
