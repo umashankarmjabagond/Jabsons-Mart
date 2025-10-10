66eafe13cdc910700573f5805abf2da9dd9b98f6
roopa_dasar <roopa.d@testyantra.in>
2025-10-10 23:08:36 +0530

    added resposive for heroSection component

--- src/features/help/HeroSection.tsx
+++ src/features/help/HeroSection.tsx
@@ -15,45 +15,47 @@ const HeroSection: React.FC<HeroProps> = () => {
   ];
 
   return (
-    <section className="w-full h-auto py-2 text-center bg-gradient-to-b from-white to-[#B3B7FF]">
-      {" "}
-      <h1 className="text-2xl md:text-3xl font-bold text-black mb-6">
+    <section className="w-full h-auto py-6 text-center bg-gradient-to-b from-white to-[#B3B7FF]">
+      <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-black mb-4 sm:mb-6">
         {HELP_PAGE_TEXT.TITLE}
       </h1>
-      <div className="flex justify-center">
-        <div className="w-full max-w-2xl flex justify-center gap-3">
+
+      <div className="flex justify-center px-4">
+        <div className="w-full max-w-2xl flex flex-col sm:flex-row justify-center gap-3">
           <Input
             type="text"
             placeholder="Search using Keywords..."
             value={searchText}
             onChange={(e) => setSearchText(e.target.value)}
-            className="p-2 w-88 rounded-sm border-none border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="p-2 w-full rounded-sm border-none focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
           <Button
             variant="blue"
             size="md"
-            className="bg-blue-800 hover:bg-blue-700 text-white px-6 py-2 rounded-sm flex items-center gap-2"
+            className="bg-blue-800 hover:bg-blue-700 text-white px-6 py-2 rounded-sm flex items-center justify-center gap-2"
           >
             <Search size="16" />
           </Button>
         </div>
       </div>
-      <div className="mt-2 flex justify-around mb-4 mx-8 transition">
-        <p className="text-base text-md font-medium text-gray-900 mt-2 ">
+
+      <div className="mt-6 px-4">
+        <p className="text-base sm:text-lg font-medium text-black-500 mb-3">
           {HELP_PAGE_TEXT.SUBTITLE}
         </p>
 
-        {supportLinks.map((link, index) => (
-          <a
-            key={index}
-            href={link.href}
-            className=" p-2 rounded-full shadow-md text-sm font-medium  transition"
-          >
-            {link.label}
-          </a>
-        ))}
+        <div className="flex flex-col sm:flex-wrap sm:flex-row justify-center items-center gap-3">
+          {supportLinks.map((link, index) => (
+            <a
+              key={index}
+              href={link.href}
+              className="px-4 py-2  shadow-md rounded-full text-sm font-medium text-gray-900  transition"
+            >
+              {link.label}
+            </a>
+          ))}
+        </div>
       </div>
-      <div></div>
     </section>
   );
 };
