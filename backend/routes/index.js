24ab6fbc47d65e5136220e374181bfd93ab435b5
umashankarmjabagond <umashankarjabagond@gmail.com>
2026-01-28 10:55:10 +0530

    fix: remove the backend from this repo

--- backend/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const authRoutes = require("./authRoute/authRoutes.js");
-const userRoutes = require("./userRoute/userRoutes.js");
-
-const router = express.Router();
-router.use("/auth", authRoutes);
-router.use("/user", userRoutes);
-
-module.exports = router;
