b9e2f4678ca70adb5df38d52dd06cdfdc3c79c28
umashankarmjabagond <umashankarjabagond@gmail.com>
2026-04-09 17:43:10 +0530

    feat : add protected route and after token expiry also go to login page

--- src/app/router/ProtectedRoute.tsx
+++ src/app/router/ProtectedRoute.tsx
@@ -1,12 +1,28 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+
+function isTokenExpired(token: string) {
+  try {
+    const payload = JSON.parse(atob(token.split(".")[1]));
+    return payload.exp * 1000 < Date.now();
+  } catch {
+    return true;
+  }
+}
 
 export default function ProtectedRoute() {
-  //   const user = localStorage.getItem("user");
-  // const parsedUser = user ? JSON.parse(user) : null;
-  // const isAuthenticated = parsedUser?.token;
+  const location = useLocation();
 
-  // if (!isAuthenticated) {
-  //     return <Navigate to="/auth/login" replace />;
-  // }
+  const user = localStorage.getItem("user");
+  const parsedUser = user ? JSON.parse(user) : null;
+
+  const token = parsedUser?.token;
+
+  const isAuthenticated = token && !isTokenExpired(token);
+
+  if (!isAuthenticated) {
+    localStorage.removeItem("user");
+    return <Navigate to="/auth/login" replace state={{ from: location }} />;
+  }
+
   return <Outlet />;
 }
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,29 +1,34 @@
 import axios from "axios";
-import type { InternalAxiosRequestConfig } from "axios";
-// eslint-disable-next-line
 
 const baseURL = import.meta.env.VITE_API_BASE_URL;
 
-// Create Axios instance with base URL from env
 const API = axios.create({
   baseURL,
 });
 
-// Request interceptor to attach token (if any) from localStorage
-API.interceptors.request.use(
-  (config: InternalAxiosRequestConfig) => {
-    const userStr = localStorage.getItem("user");
-    const user = userStr ? JSON.parse(userStr) : null;
-    if (user?.token) {
-      // eslint-disable-next-line
-      config.headers = config.headers || ({} as any);
+API.interceptors.request.use((config) => {
+  const userStr = localStorage.getItem("user");
+  const user = userStr ? JSON.parse(userStr) : null;
 
-      config.headers.Authorization = `Bearer ${user.token}`;
+  if (user?.token) {
+    config.headers = config.headers || {};
+    config.headers.Authorization = `Bearer ${user.token}`;
+  }
+
+  return config;
+});
+
+API.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401 || error.response?.status === 403) {
+      localStorage.removeItem("user");
+
+      window.location.replace("/auth/login");
     }
 
-    return config;
+    return Promise.reject(error);
   },
-  (error) => Promise.reject(error),
 );
 
 export default API;
